4a567ed36b36545947b02d83180690d52063a345
Loren Halmick <loren@jewelsbranch.com>
2023-09-28 13:34:15 -0500

    three states_buttons

--- Project_1/Project1/Project1.js
+++ Project_1/Project1/Project1.js
@@ -1,26 +1,62 @@
-//let colors = [];
+//colBrown = (color(69, 39, 1));
+//colMedGreen = (color(148, 155, 87));
+//colBeige = (color(165, 116, 75));
+//colBrick = (color(133, 50, 0));
+//colBone = (color(189, 175, 148));
+//colMoss = (color(76, 67, 24));
+//colTan = (color(180, 148, 125));
+//colPurple = (color(59, 36, 44));
+//colMossHigh = (color(132, 124, 39));
+
+let dreamButton, wakeButton, restartButton;
+let state = 0;
 
 function setup() {
   createCanvas(1920, 1080);
-  background(150);
   
- // let col = {
- //   (random('colBrown', 'colMedGreen', 'colBeige', 'colBrick', 'colBone', 'colMoss', 'colTan', 'colPurple', 'colMossHigh')) 
- // };
-//  colors.push(col);
+  //dreamButton
+  dreamButton = createButton('Dream');
+  dreamButton.position(930, 510);
+  dreamButton.mousePressed(nextPage);
+  
+  //wakeButton
+  wakeButton = createButton('Wake Up');
+  wakeButton.position(930, 510);
+  wakeButton.mousePressed(nextPage);
+  
+  //restartButton
+  restartButton = createButton('Refresh to Restart');
+  restartButton.position(930, 510);
+  restartButton.mousePressed(nextPage);
+
 }
 
 function draw() {
-   let colBrown = (color(69, 39, 1));
-  let colMedGreen = (color(148, 155, 87));
-  let colBeige = (color(165, 116, 75));
-  let colBrick = (color(133, 50, 0));
-  let colBone = (color(189, 175, 148));
-  let colMoss = (color(76, 67, 24));
-  let colTan = (color(180, 148, 125));
-  let colPurple = (color(59, 36, 44));
-  let colMossHigh = (color(132, 124, 39));
   
-  fill(random(colBrown, colMedGreen, colBeige, colBrick, colBone, colMoss, colTan, colPurple, colMossHigh));
-  rect(0, 0, 200, 200);
+  if(state === 0) {
+    dreamButton.show();
+    wakeButton.hide();
+    restartButton.hide();
+  } else if (state === 1) {
+    dreamButton.hide();
+    wakeButton.show();
+    restartButton.hide();
+  } else if(state === 2) {
+    dreamButton.hide();
+    wakeButton.hide();
+    restartButton.show();
+  }
+}
+
+function nextPage() {
+  state = (state +1) % 3;
+  
+  if (state === 0) {
+    background(230);
+  } else if(state === 1) {
+    background(150);
+  } else if(state === 2) {
+    background(100);
+  }
+  
 }
